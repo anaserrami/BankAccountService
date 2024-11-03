4660d80724b2371f27f13375604c766b75617cac
anaserrami <ramianas314@gmail.com>
2024-11-03 22:14:53 +0100

    modify service layer

--- src/main/java/com/errami/bankaccountservice/service/AccountService.java
+++ src/main/java/com/errami/bankaccountservice/service/AccountService.java
@@ -2,8 +2,13 @@ package com.errami.bankaccountservice.service;
 
 import com.errami.bankaccountservice.dto.BankAccountRequestDTO;
 import com.errami.bankaccountservice.dto.BankAccountResponseDTO;
-import com.errami.bankaccountservice.entities.BankAccount;
+
+import java.util.List;
 
 public interface AccountService {
+    List<BankAccountResponseDTO> getAllAccounts();
+    BankAccountResponseDTO getAccountById(String id);
     BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO);
+    BankAccountResponseDTO updateAccount(String id, BankAccountRequestDTO requestDTO);
+    void deleteAccount(String id);
 }
---
+++ src/main/java/com/errami/bankaccountservice/service/AccountServiceImpl.java
@@ -0,0 +1,66 @@
+package com.errami.bankaccountservice.service;
+
+import com.errami.bankaccountservice.dto.BankAccountRequestDTO;
+import com.errami.bankaccountservice.dto.BankAccountResponseDTO;
+import com.errami.bankaccountservice.entities.BankAccount;
+import com.errami.bankaccountservice.mappers.AccountMapper;
+import com.errami.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class AccountServiceImpl implements AccountService{
+    private BankAccountRepository bankAccountRepository;
+    private AccountMapper accountMapper;
+
+    public AccountServiceImpl(BankAccountRepository bankAccountRepository, AccountMapper accountMapper) {
+        this.bankAccountRepository = bankAccountRepository;
+        this.accountMapper = accountMapper;
+    }
+
+    @Override
+    public List<BankAccountResponseDTO> getAllAccounts() {
+        return bankAccountRepository.findAll().stream()
+                .map(accountMapper::fromBankAccount)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public BankAccountResponseDTO getAccountById(String id) {
+        BankAccount bankAccount = bankAccountRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Bank account with id %s not found", id)));
+        return accountMapper.fromBankAccount(bankAccount);
+    }
+
+    @Override
+    public BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO) {
+        BankAccount bankAccount = accountMapper.toBankAccount(bankAccountDTO);
+        BankAccount savedBankAccount = bankAccountRepository.save(bankAccount);
+        BankAccountResponseDTO bankAccountResponseDTO = accountMapper.fromBankAccount(savedBankAccount);
+        return bankAccountResponseDTO;
+    }
+
+    @Override
+    public BankAccountResponseDTO updateAccount(String id, BankAccountRequestDTO requestDTO) {
+        BankAccount bankAccount = bankAccountRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Bank account with id %s not found", id)));
+
+        if (requestDTO.getBalance() != null) bankAccount.setBalance(requestDTO.getBalance());
+        if (requestDTO.getCurrency() != null) bankAccount.setCurrency(requestDTO.getCurrency());
+        if (requestDTO.getType() != null) bankAccount.setType(requestDTO.getType());
+        bankAccount.setCreatedAt(new Date());
+
+        BankAccount updatedBankAccount = bankAccountRepository.save(bankAccount);
+        return accountMapper.fromBankAccount(updatedBankAccount);
+    }
+
+    @Override
+    public void deleteAccount(String id) {
+        bankAccountRepository.deleteById(id);
+    }
+}
